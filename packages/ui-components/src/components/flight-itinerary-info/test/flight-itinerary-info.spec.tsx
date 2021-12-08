16e8adca6649df4d97ac769df308465462dff67a
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2021-12-08 13:49:28 +0200

    feat(onc-flight-itinerary-info): add language property

--- packages/ui-components/src/components/flight-itinerary-info/test/flight-itinerary-info.spec.tsx
+++ packages/ui-components/src/components/flight-itinerary-info/test/flight-itinerary-info.spec.tsx
@@ -25,7 +25,22 @@ describe("onc-flight-itinerary-info", () => {
     expect(page.root).toEqualHtml(`
       <onc-flight-itinerary-info itinerary-oncarbon-id="dummyid">
         <mock:shadow-root>
-          <iframe frameborder="0" src="https://api.oncarbon.app/v1/flights/flight-itineraries/embed/dummyid" style="height: 360px;"></iframe>
+          <iframe frameborder="0" src="https://api.oncarbon.app/v1/flights/flight-itineraries/embed/dummyid?lang=en" style="height: 360px;"></iframe>
+        </mock:shadow-root>
+      </onc-flight-itinerary-info>
+    `);
+  });
+
+  it("renders the full component when itinerary id and language are given", async () => {
+    const page = await newSpecPage({
+      components: [FlightItineraryInfo],
+      html: `<onc-flight-itinerary-info itinerary-oncarbon-id="dummyid" language="fi"></onc-flight-itinerary-info>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <onc-flight-itinerary-info itinerary-oncarbon-id="dummyid" language="fi">
+        <mock:shadow-root>
+          <iframe frameborder="0" src="https://api.oncarbon.app/v1/flights/flight-itineraries/embed/dummyid?lang=fi" style="height: 360px;"></iframe>
         </mock:shadow-root>
       </onc-flight-itinerary-info>
     `);
