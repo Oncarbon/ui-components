2eeb406a514cc735fb5d9ab2ff0d2b635d789049
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2022-09-20 14:13:15 +0300

    style(react): don't run prettier on autogenerated react components

--- packages/ui-components-react/src/components/stencil-generated/index.ts
+++ packages/ui-components-react/src/components/stencil-generated/index.ts
@@ -1,18 +1,12 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from "./react-component-lib";
+import { createReactComponent } from './react-component-lib';
 
-import type { JSX } from "@oncarbon/ui-components";
+import type { JSX } from '@oncarbon/ui-components';
 
-import { defineCustomElements } from "@oncarbon/ui-components/loader";
+import { defineCustomElements } from '@oncarbon/ui-components/loader';
 
 defineCustomElements();
-export const OncFlightItineraryInfo = /*@__PURE__*/ createReactComponent<
-  JSX.OncFlightItineraryInfo,
-  HTMLOncFlightItineraryInfoElement
->("onc-flight-itinerary-info");
-export const OncFlightItineraryInfoPopover = /*@__PURE__*/ createReactComponent<
-  JSX.OncFlightItineraryInfoPopover,
-  HTMLOncFlightItineraryInfoPopoverElement
->("onc-flight-itinerary-info-popover");
+export const OncFlightItineraryInfo = /*@__PURE__*/createReactComponent<JSX.OncFlightItineraryInfo, HTMLOncFlightItineraryInfoElement>('onc-flight-itinerary-info');
+export const OncFlightItineraryInfoPopover = /*@__PURE__*/createReactComponent<JSX.OncFlightItineraryInfoPopover, HTMLOncFlightItineraryInfoPopoverElement>('onc-flight-itinerary-info-popover');
